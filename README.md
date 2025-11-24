f80a419b8a6b2cf56545417a930f0449441062ce
Yanna <yannajask@gmail.com>
2025-11-24 18:39:41 -0500

    add dielectrics

---
+++ README.md
@@ -0,0 +1,1 @@
+[_Ray Tracing in One Weekend_](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
--- src/main.cc
+++ src/main.cc
@@ -10,12 +10,14 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(colour(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(colour(0.1, 0.2, 0.5));
-    auto material_left = make_shared<metal>(colour(0.8, 0.8, 0.8));
-    auto material_right = make_shared<metal>(colour(0.8, 0.6, 0.2));
+    auto material_left = make_shared<dieletric>(1.50);
+    auto material_bubble = make_shared<dieletric>(1.00 / 1.50);
+    auto material_right = make_shared<metal>(colour(0.8, 0.6, 0.2), 1.0);
 
     scene.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     scene.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
     scene.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    scene.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
     scene.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     camera cam;
