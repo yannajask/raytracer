654ba410f1ad2e910f5f195aa7b0640e3aa527c5
Yanna <yannajask@gmail.com>
2025-11-29 22:55:35 -0500

    move rng outside of random function call and add optimization flag

--- src/utils.h
+++ src/utils.h
@@ -11,6 +11,9 @@
 using std::make_shared;
 using std::shared_ptr;
 
+inline thread_local std::mt19937 rng(std::random_device{}());
+inline thread_local std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
@@ -19,9 +22,7 @@ inline double degrees_to_radians(double degrees) {
 }
 
 inline double random_double() {
-    thread_local std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    thread_local std::mt19937 generator;
-    return distribution(generator);
+    return distribution(rng);
 }
 
 inline double random_double(double min, double max) {
